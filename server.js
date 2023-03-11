b641c3750452f8e57624c703f17aa6bdc3ef02e4
tuan-minh <tuanminh20194334@gmail.com>
2023-03-11 09:11:44 +0700

    xxx

--- server.js
+++ server.js
@@ -22,32 +22,32 @@ app.get('/', (req, res) => {
     res.send(`Listening on ${PORT}`);
 })
 
-app.post('/api/get-all-users', getAllUsers);
-app.post('/api/get-user-by-id', getUserById);
-app.post('/api/add-new-user', addNewUser);
-app.post('/api/delete-user-by-id', deleteUserById);
-app.post('/api/update-user-by-id', updateUserById);
+app.post('/get-all-users', getAllUsers);
+app.post('/get-user-by-id', getUserById);
+app.post('/add-new-user', addNewUser);
+app.post('/delete-user-by-id', deleteUserById);
+app.post('/update-user-by-id', updateUserById);
 
-app.post('/api/get-all-trucks', getAllTrucks);
-app.post('/api/get-truck-by-id', getTruckById);
-app.post('/api/add-new-truck', addNewTruck);
-app.post('/api/delete-truck-by-id', deleteTruckById);
-app.post('/api/update-truck-by-id', updateTruckById);
+app.post('/get-all-trucks', getAllTrucks);
+app.post('/get-truck-by-id', getTruckById);
+app.post('/add-new-truck', addNewTruck);
+app.post('/delete-truck-by-id', deleteTruckById);
+app.post('/update-truck-by-id', updateTruckById);
 
-app.post('/api/get-all-track-records', getAllTrackRecords);
-app.post('/api/get-track-record-by-id', getTrackRecordById);
-app.post('/api/add-new-track-record', addNewTrackRecord);
-app.post('/api/delete-track-record-by-id', deleteTrackRecordById);
-app.post('/api/update-track-record-by-id', updateTrackRecordById);
+app.post('/get-all-track-records', getAllTrackRecords);
+app.post('/get-track-record-by-id', getTrackRecordById);
+app.post('/add-new-track-record', addNewTrackRecord);
+app.post('/delete-track-record-by-id', deleteTrackRecordById);
+app.post('/update-track-record-by-id', updateTrackRecordById);
 
-app.post('/api/get-all-products', getAllProducts);
-app.post('/api/get-product-by-id', getProductById);
-app.post('/api/add-new-product', addNewProduct);
-app.post('/api/delete-product-by-id', deleteProductById);
-app.post('/api/update-product-by-id', updateProductById);
+app.post('/get-all-products', getAllProducts);
+app.post('/get-product-by-id', getProductById);
+app.post('/add-new-product', addNewProduct);
+app.post('/delete-product-by-id', deleteProductById);
+app.post('/update-product-by-id', updateProductById);
 
-app.post('/api/get-all-containers', getAllContainers);
-app.post('/api/get-container-by-id', getContainerById);
-app.post('/api/add-new-container', addNewContainer);
-app.post('/api/delete-container-by-id', deleteContainerById);
-app.post('/api/update-container-by-id', updateContainerById);
+app.post('/get-all-containers', getAllContainers);
+app.post('/get-container-by-id', getContainerById);
+app.post('/add-new-container', addNewContainer);
+app.post('/delete-container-by-id', deleteContainerById);
+app.post('/update-container-by-id', updateContainerById);
